c16e5b147194a1f87e6ef0fc92ea63f20350ee61
siramkalyan <siramkalyan@gmail.com>
2023-12-09 21:28:57 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/siramkalyan/",
+  base: "/3D_portfolio/",
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
